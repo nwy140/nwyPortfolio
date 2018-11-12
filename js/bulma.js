cc8c10f50d77eb2487c492965b587d39e4a1f0a2
Nwy <nwy140@gmail.com>
2018-11-12 13:23:34 +0800

    Added Html version of resume

--- js/bulma.js
+++ js/bulma.js
@@ -12,3 +12,18 @@
 })();
 
 
+    <script type="text/javascript">
+      (function() {
+        var burger = document.querySelector('.burger');
+        var nav = document.querySelector('#'+burger.dataset.target);
+        burger.addEventListener('click', function(){
+          burger.classList.toggle('is-active');
+          nav.classList.toggle('is-active');
+
+        });
+      })();
+      var dropdown = document.querySelector('.dropdown');
+dropdown.addEventListener('click', function(event) {
+  event.stopPropagation();
+  dropdown.classList.toggle('is-active');
+});
